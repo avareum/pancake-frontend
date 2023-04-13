e626673bf3c8fd0ee63045ec37395ac59c52abaf
katopz <katopz@gmail.com>
2023-04-13 14:41:12 +0700

    fix: wrong type

--- apps/web/src/views/Admin/components/types.ts
+++ apps/web/src/views/Admin/components/types.ts
@@ -27,7 +27,7 @@ export interface DeployNavTokeModalBodyPropsType {
 }
 
 export interface DeployNavTokeModalValidator {
-  isValidAmount: boolean
+  isValidName: boolean
   isValidSymbol: boolean
 }
 
